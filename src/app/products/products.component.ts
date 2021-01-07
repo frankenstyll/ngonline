cc56c889dab587b85cffa77e980132e89687f19b
kampon <kampon.nontapap@krungthai.com>
2021-01-07 18:41:11 +0700

    show data from server

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,7 @@
+import { ProductService } from './shared/product.service';
 import { Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './shared/product.model';
 
 @Component({
   selector: 'app-products',
@@ -7,11 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  
+  private products : Product[];
 
-  constructor(private title: Title) { }
+  constructor(
+    private title: Title ,
+    private productService : ProductService) { 
+
+  }
 
   ngOnInit() {
     this.title.setTitle("Products")
+    
+    this.reloadProduct();
+  }
+
+  private reloadProduct(){
+    this.productService.getProducts().subscribe(
+
+      (productsData) => { //productsData คือ object ที่รับข้อมูลมา
+        this.products = productsData;
+      }
+    );
   }
 
 }
--- src/app/products/shared/product.service.ts
+++ src/app/products/shared/product.service.ts
@@ -1,9 +1,17 @@
+import { Product } from './product.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private productUrl = 'https://codingthailand.com/api/get_courses.php';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  public getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.productUrl);
+  }
 }
